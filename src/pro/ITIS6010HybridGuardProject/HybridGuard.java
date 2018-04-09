1ecfea05bc42513541ef01eeb47f79347690a73d
fhamdan <fhamdan14@gmail.com>
2018-04-09 17:05:48 -0400

    Test
    Test

--- src/pro/ITIS6010HybridGuardProject/HybridGuard.java
+++ src/pro/ITIS6010HybridGuardProject/HybridGuard.java
@@ -5,17 +5,80 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 
-public class HybridGuard extends AnAction {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.Calendar;
+import javax.swing.*;
 
+public class HybridGuard extends AnAction implements ToolWindowFactory{
+
+    private JButton refreshToolWindowButton;
+    private JButton hideToolWindowButton;
+    private JLabel currentDate;
+    private JLabel currentTime;
+    private JLabel timeZone;
+    private JPanel myToolWindowContent;
+
+    private ToolWindow myToolWindow;
     public HybridGuard()
     {
 
+        hideToolWindowButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                myToolWindow.hide(null);
+            }
+        });
+        refreshToolWindowButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                HybridGuard.this.currentDateTime();
+            }
+        });
     }
 
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
-        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
-        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+        try {
+            File core_flashJax = new File(project.getBasePath()+"\\assets\\www\\js\\core-flashjax.js");
+            FileWriter fw = new FileWriter(core_flashJax);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            FileReader fr = new FileReader("/resources/core-flashjax.js");
+            BufferedReader br = new BufferedReader(fr);
+
+            if(!core_flashJax.exists())
+            {
+                core_flashJax.createNewFile();
+            }
+            String line ="";
+            while((line =br.readLine())!=null)
+            {
+                bw.write(line);
+            }
+        }
+        catch(IOException ioe)
+        {
+
+        }
+        //createToolWindowContent(project,myToolWindow);
     }
+
+    public  void createToolWindowContent(Project project, ToolWindow toolWindow)
+    {
+        myToolWindow = toolWindow;
+
+        this.currentDateTime();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindowContent,"",false);
+        toolWindow.getContentManager().addContent(content);
+    }
+    public void currentDateTime() {
+        currentDate.setText("aa");
+    }
+
 }
