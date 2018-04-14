d8517cc81352318acd8ed2c056a410f04ebf333c
DarthVitrial <redherochildboss@gmail.com>
2018-04-14 18:34:44 -0400

    gui work

--- src/pro/ITIS6010HybridGuardProject/GUI3.java
+++ src/pro/ITIS6010HybridGuardProject/GUI3.java
@@ -6,6 +6,12 @@ public class GUI3 extends JDialog{
     private JButton back;
     private JButton finish;
     private JTextArea describewrx;
+    private JCheckBox readCheckBox;
+    private JCheckBox writeCheckBox;
+    private JCheckBox executeCheckBox;
+    private JCheckBox readCheckBox1;
+    private JCheckBox writeCheckBox1;
+    private JCheckBox executeCheckBox1;
 
 
 }
