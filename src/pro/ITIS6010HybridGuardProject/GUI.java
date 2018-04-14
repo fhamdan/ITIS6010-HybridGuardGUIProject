e38b252529505cb578def8f2f5db95964518aec7
DarthVitrial <redherochildboss@gmail.com>
2018-04-14 17:41:17 -0400

    plugin.xml updates

--- src/pro/ITIS6010HybridGuardProject/GUI.java
+++ src/pro/ITIS6010HybridGuardProject/GUI.java
@@ -1,13 +1,13 @@
 package pro.ITIS6010HybridGuardProject;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class GUI {
+public class GUI{
     private JPanel Window1;
     private JButton Next;
     private JTextArea explainPrincipalsHereTextArea;
     private JTable table1;
-
     public static void main(String[] args) {
         JFrame frame = new JFrame("GUI");
         frame.setContentPane(new GUI().Window1);
